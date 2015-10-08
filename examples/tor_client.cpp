236efea3e6d30e78813af04600fd36306f48b7f3
Antonio Vanegas <hpsaturn@gmail.com>
2015-10-08 01:43:48 -0500

    add title for oled screen and scroll text function

--- examples/tor_client.cpp
+++ examples/tor_client.cpp
@@ -29,7 +29,6 @@
 // Instantiate the display
 ArduiPi_OLED display;
 
-
 // Config Option
 struct s_opts
 {
@@ -42,206 +41,10 @@ int sleep_divisor = 1 ;
 // default options values
 s_opts opts = {
     OLED_ADAFRUIT_SPI_128x32,	// Default oled
-    false										// Not verbose
+    false						// Not verbose
 };
 
-#define NUMFLAKES 10
-#define XPOS 0
-#define YPOS 1
-#define DELTAY 2
-
-#define LOGO16_GLCD_HEIGHT 16 
-#define LOGO16_GLCD_WIDTH  16 
-static unsigned char logo16_glcd_bmp[] =
-{ 0b00000000, 0b11000000,
-    0b00000001, 0b11000000,
-    0b00000001, 0b11000000,
-    0b00000011, 0b11100000,
-    0b11110011, 0b11100000,
-    0b11111110, 0b11111000,
-    0b01111110, 0b11111111,
-    0b00110011, 0b10011111,
-    0b00011111, 0b11111100,
-    0b00001101, 0b01110000,
-    0b00011011, 0b10100000,
-    0b00111111, 0b11100000,
-    0b00111111, 0b11110000,
-    0b01111100, 0b11110000,
-    0b01110000, 0b01110000,
-    0b00000000, 0b00110000 };
-
-
-void testdrawbitmap(const uint8_t *bitmap, uint8_t w, uint8_t h) {
-    uint8_t icons[NUMFLAKES][3];
-    srandom(666);     // whatever seed
-
-    // initialize
-    for (uint8_t f=0; f< NUMFLAKES; f++) {
-        icons[f][XPOS] = random() % display.width();
-        icons[f][YPOS] = 0;
-        icons[f][DELTAY] = random() % 5 + 1;
-
-        printf("x: %d", icons[f][XPOS]);
-        printf("y: %d", icons[f][YPOS]);
-        printf("dy: %d\n", icons[f][DELTAY]);
-    }
-
-    while (1) {
-        // draw each icon
-        for (uint8_t f=0; f< NUMFLAKES; f++) {
-            display.drawBitmap(icons[f][XPOS], icons[f][YPOS], logo16_glcd_bmp, w, h, WHITE);
-        }
-        display.display();
-        usleep(100000/sleep_divisor);
-
-        // then erase it + move it
-        for (uint8_t f=0; f< NUMFLAKES; f++) {
-            display.drawBitmap(icons[f][XPOS], icons[f][YPOS],  logo16_glcd_bmp, w, h, BLACK);
-            // move it
-            icons[f][YPOS] += icons[f][DELTAY];
-            // if its gone, reinit
-            if (icons[f][YPOS] > display.height()) {
-                icons[f][XPOS] = random() % display.width();
-                icons[f][YPOS] = 0;
-                icons[f][DELTAY] = random() % 5 + 1;
-            }
-        }
-    }
-}
-
-
-void testdrawchar(void) {
-    display.setTextSize(1);
-    display.setTextColor(WHITE);
-    display.setCursor(0,0);
-
-    for (uint8_t i=0; i < 168; i++) {
-        if (i == '\n') continue;
-        display.write(i);
-        if ((i > 0) && (i % 21 == 0))
-            display.print("\n");
-    }    
-    display.display();
-}
-
-void testdrawcircle(void) {
-    for (int16_t i=0; i<display.height(); i+=2) {
-        display.drawCircle(display.width()/2, display.height()/2, i, WHITE);
-        display.display();
-    }
-}
-
-void testfillrect(void) {
-    uint8_t color = 1;
-    for (int16_t i=0; i<display.height()/2; i+=3) {
-        // alternate colors
-        display.fillRect(i, i, display.width()-i*2, display.height()-i*2, color%2);
-        display.display();
-        color++;
-    }
-}
-
-void testdrawtriangle(void) {
-    for (int16_t i=0; i<min(display.width(),display.height())/2; i+=5) {
-        display.drawTriangle(display.width()/2, display.height()/2-i,
-                display.width()/2-i, display.height()/2+i,
-                display.width()/2+i, display.height()/2+i, WHITE);
-        display.display();
-    }
-}
-
-void testfilltriangle(void) {
-    uint8_t color = WHITE;
-    for (int16_t i=min(display.width(),display.height())/2; i>0; i-=5) {
-        display.fillTriangle(display.width()/2, display.height()/2-i,
-                display.width()/2-i, display.height()/2+i,
-                display.width()/2+i, display.height()/2+i, WHITE);
-        if (color == WHITE) color = BLACK;
-        else color = WHITE;
-        display.display();
-    }
-}
-
-void testdrawroundrect(void) {
-    for (int16_t i=0; i<display.height()/2-2; i+=2) {
-        display.drawRoundRect(i, i, display.width()-2*i, display.height()-2*i, display.height()/4, WHITE);
-        display.display();
-    }
-}
-
-void testfillroundrect(void) {
-    uint8_t color = WHITE;
-    for (int16_t i=0; i<display.height()/2-2; i+=2) {
-        display.fillRoundRect(i, i, display.width()-2*i, display.height()-2*i, display.height()/4, color);
-        if (color == WHITE) color = BLACK;
-        else color = WHITE;
-        display.display();
-    }
-}
-
-void testdrawrect(void) {
-    for (int16_t i=0; i<display.height()/2; i+=2) {
-        display.drawRect(i, i, display.width()-2*i, display.height()-2*i, WHITE);
-        display.display();
-    }
-}
-
-void testdrawline() {  
-    for (int16_t i=0; i<display.width(); i+=4) {
-        display.drawLine(0, 0, i, display.height()-1, WHITE);
-        display.display();
-    }
-    for (int16_t i=0; i<display.height(); i+=4) {
-        display.drawLine(0, 0, display.width()-1, i, WHITE);
-        display.display();
-    }
-    usleep(250000/sleep_divisor);
-
-    display.clearDisplay();
-    for (int16_t i=0; i<display.width(); i+=4) {
-        display.drawLine(0, display.height()-1, i, 0, WHITE);
-        display.display();
-    }
-    for (int16_t i=display.height()-1; i>=0; i-=4) {
-        display.drawLine(0, display.height()-1, display.width()-1, i, WHITE);
-        display.display();
-    }
-    usleep(250000/sleep_divisor);
-
-    display.clearDisplay();
-    for (int16_t i=display.width()-1; i>=0; i-=4) {
-        display.drawLine(display.width()-1, display.height()-1, i, 0, WHITE);
-        display.display();
-    }
-    for (int16_t i=display.height()-1; i>=0; i-=4) {
-        display.drawLine(display.width()-1, display.height()-1, 0, i, WHITE);
-        display.display();
-    }
-    usleep(250000/sleep_divisor);
-
-    display.clearDisplay();
-    for (int16_t i=0; i<display.height(); i+=4) {
-        display.drawLine(display.width()-1, 0, 0, i, WHITE);
-        display.display();
-    }
-    for (int16_t i=0; i<display.width(); i+=4) {
-        display.drawLine(display.width()-1, 0, i, display.height()-1, WHITE); 
-        display.display();
-    }
-    usleep(250000/sleep_divisor);
-}
-
-void testscrolltext(void) {
-    display.setTextSize(2);
-    display.setTextColor(WHITE);
-    display.setCursor(10,0);
-    display.clearDisplay();
-
-    if (opts.oled == OLED_SH1106_I2C_128x64)
-        display.print("No scroll\non SH1106");
-    else
-        display.print("scroll");
-    display.display();
+void scrollText(){
 
     display.startscrollright(0x00, 0x0F);
     sleep(2);
@@ -256,8 +59,8 @@ void testscrolltext(void) {
     display.startscrolldiagleft(0x00, 0x07);
     sleep(2);
     display.stopscroll();
-}
 
+}
 
 /* ======================================================================
 Function: usage
@@ -407,72 +210,17 @@ int main(int argc, char **argv)
     }
 
     display.clearDisplay();
-
-    // draw the first ~12 characters in the font
-    testdrawchar();
-    display.display();
-    sleep(2);
-    display.clearDisplay();
-
     // text display tests
     display.setTextSize(1);
-    display.setTextColor(WHITE);
+    display.setTextColor(BLACK,WHITE);
     display.setCursor(0,0);
-    display.print("Hello, world!\n");
-    display.setTextColor(BLACK, WHITE); // 'inverted' text
-    display.printf("%f\n", 3.141592);
-    display.setTextSize(2);
-    display.setTextColor(WHITE);
-    display.printf("0x%8X\n", 0xDEADBEEF);
-    display.display();
-    sleep(2); 
-
-
-    // horizontal bargraph tests
-    display.clearDisplay();
+    display.print("=Tor=");
     display.setTextSize(1);
+    display.setCursor(36,0);
     display.setTextColor(WHITE);
-    for ( i =0 ; i<=100 ; i++)
-    {
-        display.clearDisplay();
-        display.setCursor(0,0);
-        display.print("Gauge Graph!\n");
-        display.printf("  %03d %%", i);
-        display.drawHorizontalBargraph(0,16, (int16_t) display.width(),16,1, i);
-        display.display();
-        usleep(25000/sleep_divisor);
-    }
-
-    // vertical bargraph tests
-    display.clearDisplay();
-    display.setTextSize(1);
-    display.setTextColor(WHITE);
-    for ( i =0 ; i<=100 ; i++)
-    {
-        display.clearDisplay();
-        display.setCursor(0,0);
-        display.print("Gauge !\n");
-        display.printf("%03d %%", i);
-        display.drawVerticalBargraph(114,0,8,(int16_t) display.height(),1, i);
-        display.display();
-        usleep(25000/sleep_divisor);
-    }
-
-
-    // miniature bitmap display
-    display.clearDisplay();
-    display.drawBitmap(30, 16,  logo16_glcd_bmp, 16, 16, 1);
+    display.printf("Relay v%s", PRG_VERSION);
     display.display();
-
-    // invert the display
-    display.invertDisplay(true);
-    sleep(1); 
-    display.invertDisplay(false);
-    sleep(1); 
-
-    // draw a bitmap icon and 'animate' movement
-    testdrawbitmap(logo16_glcd_bmp, LOGO16_GLCD_HEIGHT, LOGO16_GLCD_WIDTH);
-
+    scrollText();
     // Free PI GPIO ports
     display.close();
 
